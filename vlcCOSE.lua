24723f83bc35f0967896e1e67f1a26a5dc7f273a
ikjelle <ik-jelle@hotmail.nl>
2018-12-17 17:41:50 +0100

    Forgot a dialog hide when starting the video

--- vlcCOSE.lua
+++ vlcCOSE.lua
@@ -1,5 +1,5 @@
 ------ Constants
--- BaseFolder constant, should only be set once when activated
+-- BaseFolder constant, should only be set
 BaseFolder = ""
 ------ Basic functions
  -- Print data in vlc console
@@ -318,6 +318,7 @@ end
 --
 function OpenLastPlayed()
   Configuration:PlayShow()
+  dialog:hide()
 end
 -- open startup dialog
 function openStartupDialog()
